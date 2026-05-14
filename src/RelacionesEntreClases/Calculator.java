5aa4f52574aa502971a2da1d2e6b4e3d19a42cf7
xavierncg <xavierncg17@gmail.com>
2026-05-14 18:11:36 -0400

    Se realiza feat: Relaciones Entre clases

--- src/RelacionesEntreClases/Calculator.java
+++ src/RelacionesEntreClases/Calculator.java
@@ -4,4 +4,22 @@ public class Calculator {
     public int add(int number1, int number2 ) {
         return number1 + number2;
     }
+    public int sub(int number1, int number2){
+        return number1 - number2;
+    }
+    public int multi(int number1, int number2){
+        return number1 * number2;
+    }
+    public int div(int number1, int number2){
+        return number1 / number2;
+    }
 }
+
+/**
+ *Aca estaremos trabajando Relacion de Clases
+ *
+ * Concepto:               =====Dependencia======
+ *
+ * Se crea como Clase que realizara algo para ser rutilizada.
+ * y ser llamada desde otra parte del codigo para resolver algo
+ */
