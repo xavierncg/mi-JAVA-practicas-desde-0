fdce2ca530f4739f6cf7d5d38fbad8ae2517ff92
xavierncg <xavierncg17@gmail.com>
2026-05-19 18:17:30 -0400

    Se soluciona problema de logica

--- src/ProyectoFinal/MainBancoApp.java
+++ src/ProyectoFinal/MainBancoApp.java
@@ -3,7 +3,7 @@ package ProyectoFinal;
 public class MainBancoApp {
     public static void main(String[] args) {
 
-        BankAccount cuentaBancaria = new BankAccount("Ana",700.000);
+        BankAccount cuentaBancaria = new BankAccount("Ana",700000.0);
         BankApp menu = new BankApp(cuentaBancaria);
         menu.iniciarMenu();
     }
