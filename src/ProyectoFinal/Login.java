93e0f30cede4e5b6c9bca53d6fa0306e54755978
xavierncg <xavierncg17@gmail.com>
2026-05-29 19:47:33 -0400

    fix:Se actualiza error de nombre metodo gettern y setter

---
+++ src/ProyectoFinal/Login.java
@@ -0,0 +1,11 @@
+package ProyectoFinal;
+
+public class LoginSecret {
+    private int rut;
+    private String password = null;
+
+    public LoginSecret(int rut, String password){
+        this.rut = rut;
+        this.password = password;
+    }
+}
